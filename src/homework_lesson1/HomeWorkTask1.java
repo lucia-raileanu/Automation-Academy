60947e6ca06d7872ad538f80977544ba981a9a85
LRaileanu <lucrai@yahoo.com>
2024-04-30 18:37:58 +0300

    add homeworktask1

--- src/homework_lesson1/HomeWorkTask1.java
+++ src/homework_lesson1/HomeWorkTask1.java
@@ -1,10 +1,18 @@
 package homework_lesson1;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class HomeWorkTask1 {
     public static void main(String[] args) {
-        System.out.println();
+        int n1=5;
+        int n2=1;
+        int conter=0;
+        AtomicInteger counter = new AtomicInteger();
+        while (n1>=n2){
+            n1-= 1;
+        counter.getAndIncrement();}
 
-
+        System.out.println(counter.get());
 
     }
 }
